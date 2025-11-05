4d517800ebd6e5b2efa1de33cb516c3614b05527
michalwa <michalwa2003@gmail.com>
2025-11-05 15:15:01 +0100

    use bincode

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use clap::Parser;
-use std::io::{Read, Write};
+use std::error::Error;
+use std::io::Write;
 use std::net::{SocketAddr, TcpListener, TcpStream};
 
-#[derive(Debug)]
+fn bincode_config() -> impl bincode::config::Config {
+    use bincode::config::*;
+    // Use fixed int encoding so that the protocol is easy to implement in other
+    // clients and doesn't require implementing the specific variable encoding
+    // algorithm used by bincode
+    Configuration::<BigEndian, Fixint, NoLimit>::default()
+}
+
+#[derive(Debug, bincode::Encode, bincode::Decode)]
 struct Message {
-    protocol_version: u16,
     nonce: u16,
     text: String,
 }
@@ -26,33 +35,44 @@ enum Mode {
 fn main() {
     let args = Args::parse();
 
-    let stream = match args.mode {
-        Mode::Listen { addr } => TcpListener::bind(addr).unwrap().accept().unwrap().0,
-        Mode::Connect { addr } => TcpStream::connect(addr).unwrap(),
-    };
-
-    handler(stream);
+    match args.mode {
+        Mode::Listen { addr } => {
+            let stream = TcpListener::bind(addr).unwrap().accept().unwrap().0;
+            recv_message(stream).unwrap();
+        },
+        Mode::Connect { addr } => {
+            let stream = TcpStream::connect(addr).unwrap();
+            send_message(stream).unwrap();
+        },
+    }
 }
 
-fn handler(mut stream: TcpStream) {
-    let mut protocol_version = [0; 2];
-    stream.read_exact(&mut protocol_version).unwrap();
-    let protocol_version = u16::from_be_bytes(protocol_version);
-
-    let mut data = [0; 4];
-    stream.read_exact(&mut data).unwrap();
-    let nonce = u16::from_be_bytes(data[0..2].try_into().unwrap());
-    let text_len = u16::from_be_bytes(data[2..4].try_into().unwrap()).into();
-
-    let mut text = vec![0; text_len];
-    stream.read_exact(&mut text).unwrap();
-
-    let message = Message {
-        protocol_version,
-        nonce,
-        text: String::from_utf8(text).unwrap(),
-    };
-
+fn recv_message(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    // TODO: Check protocol version
+    let _protocol_version = stream.read_u16::<BigEndian>()?;
+    let message: Message = bincode::decode_from_std_read(&mut stream, bincode_config())?;
     println!("{message:?}");
-    writeln!(stream, "siemanko").unwrap();
+
+    Ok(())
+}
+
+fn send_message(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    let message = Message {
+        nonce: u16::MAX,
+        text: "siemanko".into(),
+    };
+    stream.write_u16::<BigEndian>(1)?;
+
+    let bytes = bincode::encode_to_vec(&message, bincode_config())?;
+    println!("encoded message: {bytes:x?}");
+
+    // ```
+    // let bytes_written = bincode::encode_into_std_write(message, &mut stream, bincode_config())?;
+    // println!("wrote {bytes_written} bytes");
+    // ```
+
+    stream.write_all(&bytes)?;
+    println!("wrote {} bytes", bytes.len());
+
+    Ok(())
 }
